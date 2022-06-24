ae369660eb39d58538640c2d0d869ca4d4a1e569
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-06-24 12:02:42 +0100

    created a listings view

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,34 @@ use Illuminate\Http\Request;
 |
 */
 
+// Returns view of listings
 Route::get('/', function () {
-    return view('welcome');
+    return view('listings', [
+        'heading' => 'Latest Listings',
+        'listings' => [
+            [
+                'id' => 1,
+                'title' => 'Listing One',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Listing Two',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+            ]
+        ]
+    ]);
 });
 
+
+
+
+
+
+
+
+
+// practice
 Route::get('/hello', function(){
     return response('<h1>Hello World</h1>', 200)
         ->header('Content-Type', 'text/plain')
@@ -32,3 +56,5 @@ Route::get('/search', function(Request $request){
     return $request->name . ' ' . $request->city;
 
 });
+
+
