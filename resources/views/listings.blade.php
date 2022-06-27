615cc0ba824843a7d55776fd59ba611e37c60027
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-06-27 16:23:55 +0100

    added links to individual listings

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,9 +1,23 @@
 <h1>{{$heading}}</h1>
+
+@unless(count($listings) == 0)
+
 @foreach($listings as $listing)
     <h2>
-        {{$listing['title']}}
+        <a href="/listings/{{$listing['id']}}">{{$listing['title']}}</a>
     </h2>
     <p>
         {{$listing['description']}}
     </p>
 @endforeach
+
+    
+@else
+    <p>No listings found</p>
+@endunless
+
+
+{{-- Example of how no listings found --}}
+{{-- @if(count($listings) == 0)
+    <p>No listings</p>
+@endif  --}}
