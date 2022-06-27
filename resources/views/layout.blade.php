fa8d4d1e58c31f8992ab6ca254353f2b516a31b4
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-06-27 20:34:34 +0100

    added layout view

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta chartset="UTF-8"?
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>STEM hub</title>
+        
+    </head>
+    <body>
+        <h1>STEM hub</h1>
+        {{-- VIEW OUTPUT--}}
+        @yield('content')
+    </body>
+    </html>
