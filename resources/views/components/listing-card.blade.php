a8a5d7d8f44417eb290623654d258c769e585076
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-06-29 09:46:06 +0100

    added card for styling

--- resources/views/components/listing-card.blade.php
+++ resources/views/components/listing-card.blade.php
@@ -1,7 +1,7 @@
 @props(['listing'])
 
 
-<div class="bg-gray-50 border border-gray-200 rounded p-6">
+<x-card>
     <div class="flex">
         <img class="hidden w-48 mr-6 md:block" src="{{asset('images/no-image.png')}}" alt="" />
         <div>
@@ -32,4 +32,4 @@
             </div>
         </div>
     </div>
-</div>
+</x-card>
