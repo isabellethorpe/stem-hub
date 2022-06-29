18070943f54e2da99721fdbd4baffa334690953a
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-06-29 20:45:03 +0100

    created store listing data route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,9 @@ Route::get('/', [ListingController::class, 'index']);
 // Show Create Form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
+// Store Listing Data
+Route::post('/listings', [ListingController::class, 'store']);
+
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
