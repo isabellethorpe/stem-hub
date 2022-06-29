ee017a19ed272c682b5e868455a77086683b68e6
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-06-29 20:06:03 +0100

    basic create listing

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,13 @@ use App\Models\Listing;
 // All listings
 Route::get('/', [ListingController::class, 'index']);
 
+
+
+// Show Create Form
+Route::get('/listings/create', [ListingController::class, 'create']);
+
 // Single listing
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
-
 
 
 // Common Resource Routes (naming conventions):
