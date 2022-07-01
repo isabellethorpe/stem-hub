bda31681e002a608f90c188be9dd6b1b2779f528
Isabelle Thorpe <isabelle_thorpe@Outlook.com>
2022-07-01 12:30:01 +0100

    added edit card/link to single listing

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -35,6 +35,11 @@
                 </div>
             </div>
         </x-card>
+
+        <x-card class="mt-4 p-2 flex space-x-6">
+          <a href="/listings/{{$listing->id}}/edit">
+            <i class="fa-solid fa-pencil"></i> Edit  
+        </x-card>
     </div>
 </x-layout>
 
